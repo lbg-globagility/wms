b04aee2ad0a6568f9d1baceafdcac56054f19460
Ronaldo Dupra <rdupra.globagility@gmail.com>
2023-11-20 22:04:27 +0800

    feat(#20): Add contactcities to schema

---
+++ Database Schema/contactcities.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table dws.contactcities
+CREATE TABLE IF NOT EXISTS `contactcities` (
+  `RowID` int(11) NOT NULL AUTO_INCREMENT,
+  `ContactID` int(11) NOT NULL,
+  `CityID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`RowID`) USING BTREE,
+  KEY `FK_contact` (`ContactID`),
+  CONSTRAINT `FK_contact` FOREIGN KEY (`ContactID`) REFERENCES `contacts` (`RowID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
