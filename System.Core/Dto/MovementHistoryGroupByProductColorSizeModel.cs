93fe3ea23fcfdf771a04dfd414b46ce9068f396b
Lambert Galit <lbg.globagility@gmail.com>
2023-11-21 08:38:40 +0800

    fix: CodeMaid cleanse

--- System.Core/Dto/MovementHistoryGroupByProductColorSizeModel.cs
+++ System.Core/Dto/MovementHistoryGroupByProductColorSizeModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using WarehouseManagementSystem.Core.Entities;
-using WarehouseManagementSystem.Core.Exceptions;
 
 namespace WarehouseManagementSystem.Core.Dto
 {
