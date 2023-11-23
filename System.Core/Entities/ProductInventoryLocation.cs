2c5da3bf46c8e2f56c1563ecca3ae240dd1de6d5
Lambert Galit <lbg.globagility@gmail.com>
2023-11-23 11:09:58 +0800

    fix: substitue zero per property when accessing

--- System.Core/Entities/ProductInventoryLocation.cs
+++ System.Core/Entities/ProductInventoryLocation.cs
@@ -60,6 +60,6 @@ namespace WarehouseManagementSystem.Core.Entities
                 unitOfMeasure: unitOfMeasure,
                 unitPrice: unitPrice);
 
-        public int TotalOrderableQty => (TotalAvailableQty - TotalAllocatedQty) ?? 0;
+        public int TotalOrderableQty => (TotalAvailableQty ?? 0) - (TotalAllocatedQty ?? 0);
     }
 }
