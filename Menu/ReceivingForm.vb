e8935145f16258408d129ba7b0006137acb27cca
Ronaldo Dupra <rdupra.globagility@gmail.com>
2023-11-29 13:14:18 +0800

    feat(#25): show quantity to receive to specific user/position

--- Menu/ReceivingForm.vb
+++ Menu/ReceivingForm.vb
@@ -1300,7 +1300,8 @@ Public Class ReceivingForm
                 enableGB(fraud, legit)
                 normalHideReceivingItems(fraud)
                 enableANDvisibleMS(fraud, legit, fraud, legit, fraud)
-                getPositionView(globalpositionid, "Approve Receiving", Me)
+
+                getPositionView(globalpositionid, "Show Quantity to Receive", Me)
                 If globalupdateflg = "Y" Then
                     viewHideReceivingItems(legit)
                 Else
@@ -1360,7 +1361,7 @@ Public Class ReceivingForm
                 clearDatagrids()
                 enableGB(legit, legit)
                 normalHideReceivingItems(fraud)
-                getPositionView(globalpositionid, "Approve Receiving", Me)
+                getPositionView(globalpositionid, "Show Quantity to Receive", Me)
                 If globalupdateflg = "Y" Then
                     viewHideReceivingItems(legit)
                 Else
@@ -1418,7 +1419,7 @@ Public Class ReceivingForm
                 clearDatagrids()
                 enableGB(legit, legit)
                 normalHideReceivingItems(fraud)
-                getPositionView(globalpositionid, "Approve Receiving", Me)
+                getPositionView(globalpositionid, "Show Quantity to Receive", Me)
                 If globalupdateflg = "Y" Then
                     viewHideReceivingItems(legit)
                 Else
@@ -1474,7 +1475,7 @@ Public Class ReceivingForm
                     clearDatagrids()
                     enableGB(legit, legit)
                     normalHideReceivingItems(fraud)
-                    getPositionView(globalpositionid, "Approve Receiving", Me)
+                    getPositionView(globalpositionid, "Show Quantity to Receive", Me)
                     If globalupdateflg = "Y" Then
                         viewHideReceivingItems(legit)
                     Else
