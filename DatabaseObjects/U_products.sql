47b6baade12905ac40b93f7b325cdddd7a3c744f
Lambert Galit <lbg.globagility@gmail.com>
2023-12-12 12:32:46 +0800

    fix: drop clause based on procedure
    part 2

--- DatabaseObjects/U_products.sql
+++ DatabaseObjects/U_products.sql
@@ -12,6 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP PROCEDURE IF EXISTS `U_products`;
 -- Dumping structure for procedure dws.U_products
 DELIMITER //
 CREATE PROCEDURE `U_products`(
