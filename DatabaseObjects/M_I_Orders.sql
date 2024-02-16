c3955f2a053d01a43be40fa665501c528cc03a2b
Lambert Galit <lbg.globagility@gmail.com>
2024-02-16 15:00:02 +0800

    execute the drop command first

--- DatabaseObjects/M_I_Orders.sql
+++ DatabaseObjects/M_I_Orders.sql
@@ -12,6 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP FUNCTION IF EXISTS `M_I_Orders`;
 -- Dumping structure for function dws.M_I_Orders
 DELIMITER //
 CREATE FUNCTION `M_I_Orders`(`I_OrganizationID` INT(10),
