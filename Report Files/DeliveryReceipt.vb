345c22d497b2746f170ecd16c4b84805cafab2ff
Lambert Galit <lbg.globagility@gmail.com>
2024-05-28 17:20:45 +0800

    fix: added grand total sum of all quantity

--- Report Files/DeliveryReceipt.vb
+++ Report Files/DeliveryReceipt.vb
@@ -110,9 +110,17 @@ Public Class DeliveryReceipt
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Section5() As CrystalDecisions.CrystalReports.Engine.Section
+    Public ReadOnly Property ReportFooterSection3() As CrystalDecisions.CrystalReports.Engine.Section
         Get
             Return Me.ReportDefinition.Sections(7)
+        End Get
+    End Property
+    
+    <Browsable(false),  _
+     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
+    Public ReadOnly Property Section5() As CrystalDecisions.CrystalReports.Engine.Section
+        Get
+            Return Me.ReportDefinition.Sections(8)
         End Get
     End Property
 End Class
