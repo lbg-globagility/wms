54a2cd2dd9c36eca6205b9addd21625ab113bd20
Lambert Galit <lbg.globagility@gmail.com>
2024-11-21 14:25:15 +0800

    fix: use the pick-list~orderitem that is properly picked
    not just validate if it is the default ProductInventoryLocation

--- SubForms/LineupDelivery/DeliveryReceiptPrintOptions.vb
+++ SubForms/LineupDelivery/DeliveryReceiptPrintOptions.vb
@@ -106,7 +106,9 @@ Public Class DeliveryReceiptPrintOptions
             INNER JOIN products p ON p.RowID=pc.ProductID
             INNER JOIN productinventorylocation pil ON pil.RowID=oi.ProductInventoryLocationId
             INNER JOIN picklistorders plo ON plo.OrderItemID=oi.RowID
-            INNER JOIN picklistorderitems ploi ON ploi.PickListOrderID=plo.RowID AND ploi.`status` NOT IN ('Cancelled', 'Inactive') AND oi.ProductInventoryLocationId=ploi.ProductInventoryLocationID
+            INNER JOIN picklistorderitems ploi ON ploi.PickListOrderID=plo.RowID AND ploi.`status` NOT IN ('Cancelled', 'Inactive')
+            #AND oi.ProductInventoryLocationId=ploi.ProductInventoryLocationID
+            AND ploi.QtyPicked > 0
             WHERE lu.RowID IS NOT NULL
             AND lu.`Status` != 'Cancelled'
             {If(CheckBoxBasedOnPOnumber.Checked, String.Empty, "AND lu.RowID = @lineupRowId")}
